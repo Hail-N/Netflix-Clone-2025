39fcc6698242c5054bad1902e8a68ccbeb0f2ea9
Hailezghi Negusse <Hailezgi@8c3fdb.onmicrosoft.com>
2025-09-04 16:52:30 -0400

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+    
+    <Home/>
+   
     </div>
   );
 }
---
+++ netflix-clone/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+const Home =() =>{
+    return (
+        <div>
+            <Header/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <Footer/>
+        </div>
+    )
+}
+export default Home;
